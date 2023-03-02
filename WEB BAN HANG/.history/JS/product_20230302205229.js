595ae3af0d1ce248101d87419a89d60b5002cf74
your name <maihothienthach12a1thpt@gmail.com>
2023-03-02 23:02:44 +0700

    add chuc nang

---
+++ WEB BAN HANG/.history/JS/product_20230302205229.js
@@ -0,0 +1,5 @@
+var element1 = document.querySelector('.sell__menu .sell__menu-list:nth-child(1)')
+var element2 = document.querySelector('.sell__menu .sell__menu-list:nth-child(2)')
+var element1 = document.querySelector('.sell__menu .sell__menu-list:nth-child(1)')
+var element1 = document.querySelector('.sell__menu .sell__menu-list:nth-child(1)')
+console.log(element1)
---
+++ WEB BAN HANG/.history/JS/product_20230302205407.js
@@ -0,0 +1,10 @@
+var element1 = document.querySelector('.sell__menu .sell__menu-list:nth-child(1)')
+var element2 = document.querySelector('.sell__menu .sell__menu-list:nth-child(2)')
+var element3 = document.querySelector('.sell__menu .sell__menu-list:nth-child(3)')
+var element4 = document.querySelector('.sell__menu .sell__menu-list:nth-child(4)')
+
+var product1 = document.querySelector('.sell_product:nth-child')
+console.log(element1)
+console.log(element2)
+console.log(element3)
+console.log(element4)
